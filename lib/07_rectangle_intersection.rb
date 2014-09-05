224eed92a04ff0af1decc49a1d955b8789752e7f
Trevor Jones <trevordavidjones@gmail.com>
2014-09-05 12:44:56 -0500

    fixed indentation

--- lib/07_rectangle_intersection.rb
+++ lib/07_rectangle_intersection.rb
@@ -5,9 +5,9 @@ def rec_intersection(rect1, rect2)
   right_edge = [rect1[1][0], rect2[1][1]].max
   return rect1 if (rect2[0][0] < rect1[0][0]) && rect2[1][1] > rect1[1][1]
   return nil if rect1[1][0] < rect2[0][0] && rect1[1][1] < rect2[0][1]
-    if rect2[1][0] < rect1[1][0]
-      return [[left_edge, bottom_edge], [rect2[1][0], top_edge]]
-    else 
-      return [[left_edge, bottom_edge], [right_edge, top_edge]]
-    end
+  if rect2[1][0] < rect1[1][0]
+    return [[left_edge, bottom_edge], [rect2[1][0], top_edge]]
+  else 
+    return [[left_edge, bottom_edge], [right_edge, top_edge]]
+  end
 end
