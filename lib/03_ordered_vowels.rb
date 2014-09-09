6271e05da6f680efe47ab0e9b2e15228c56bcf83
Trevor Jones <trevordavidjones@gmail.com>
2014-09-09 11:05:45 -0500

    refactored ordered_vowels method

--- lib/03_ordered_vowels.rb
+++ lib/03_ordered_vowels.rb
@@ -1,32 +1,18 @@
+VOWELS = "aeiou"
 def ordered_vowel_word?(str)
-  vowel = "aeiou"
   array_vowel = []
-  array_letters = str.split("")
-  array_letters.each do |letter|
-    if vowel.include?(letter)
+  str.split("").each do |letter|
+    if VOWELS.include?(letter)
       array_vowel << letter
     end
   end
-  if array_vowel == array_vowel.sort
-    return true
-  else
-    return false
-  end
+  return array_vowel == array_vowel.sort
 end
 
 def ordered_vowel_words(str) 
-  if str.split("").include?(" ")
-    array_words = str.split(" ")
-    new_string = ""
-    array_words.each do |word|
-      if ordered_vowel_word?(word)
-        new_string += word + " "
-      end
-    end
-    return new_string.strip
-  elsif ordered_vowel_word?(str)
-    return str
-  else
-    return ""
+  str_array = str.split(" ")
+  str_array.each do |word|
+    str_array -= [word] unless ordered_vowel_word?(word)
   end
+  return str_array.join(" ")
 end
