43d8ef80fc3ba4a59565fa1e2eb03be3aad58d70
Trevor Jones <trevordavidjones@gmail.com>
2014-09-09 09:02:40 -0500

    refactored nearest_larger method

--- lib/00_nearest_larger.rb
+++ lib/00_nearest_larger.rb
@@ -1,21 +1,9 @@
 def nearest_larger(arr, idx) 
-  left = idx - 1
-  right = idx + 1
-  loop do
-    if arr[idx] == arr.max
-      return nil
-    elsif arr[idx] < arr[left]
-      return left
-    elsif (arr[idx] > arr[left]) && (arr[idx] > arr[right]) && left != 0
-      left = left - 1
-      return left
-    elsif (arr[idx] > arr[left]) && (arr[idx] > arr[right]) && left == 0
-      right = right + 1
-      return right
-    elsif arr[idx] < arr[right]
-      return arr[left]
+  next_highest_number = arr.max + 1
+  arr.each do |number|
+    if number > arr[idx] && number < next_highest_number
+      next_highest_number = number
     end
-    left = idx - 1
-    right = idx + 1
   end
+  arr.index(next_highest_number)
 end
