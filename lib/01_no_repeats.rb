b6b7a952eb12d54ca65bce46533eadaac8312f73
Trevor Jones <trevordavidjones@gmail.com>
2014-09-09 09:43:18 -0500

    refactored no_repeats method

--- lib/01_no_repeats.rb
+++ lib/01_no_repeats.rb
@@ -1,19 +1,10 @@
 def no_repeat?(year)
   array_years = year.to_s.split("")
-  if array_years.uniq.length == array_years.length
-    return true
-  else 
-    return false
-  end
+  return array_years.uniq.length == array_years.length
 end
 
 def no_repeats(year_start, year_end)
-  no_repeat_years = []
-  years = (year_start..year_end) 
-    years.each do |year|
-      if no_repeat?(year)
-        no_repeat_years << year
-      end
-    end
-  return no_repeat_years
+  (year_start..year_end).each_with_object([]) do |year, array|
+    array << year if no_repeat?(year)
+  end
 end
